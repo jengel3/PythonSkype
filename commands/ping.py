1f1a3a67ff91f92765e5322924b75396c37439c0
Jake <jake0oo0andminecraft@gmail.com>
2014-06-26 15:08:57 -0500

    Add ping command

---
+++ commands/ping.py
@@ -0,0 +1,19 @@
+from util.plugin import command
+import subprocess
+import os
+
+
+@command(name='ping', help='Ping an IP, with the output as you would see it in the command line.')
+def ping_command(chat, message, args, sender):
+    if len(args) == 0:
+        chat.SendMessage("Provide an IP to ping.")
+        return
+    ip = args[0]
+    if os.name == 'nt':
+        process = subprocess.Popen('ping {} -n 3 -w 2000'.format(ip), stdout=subprocess.PIPE)
+        out, err = process.communicate()
+        chat.SendMessage(out)
+    else:
+        process = subprocess.Popen('ping {} -c 3 -w 2000'.format(ip), stdout=subprocess.PIPE)
+        out, err = process.communicate()
+        chat.SendMessage(out)
