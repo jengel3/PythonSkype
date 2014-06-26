efd884e8e629398d4df2b9ce9126cd5738b76e95
Jake <jake0oo0andminecraft@gmail.com>
2014-06-25 20:50:34 -0500

    Spam plugin for fun

---
+++ commands/spam.py
@@ -0,0 +1,23 @@
+from util.plugin import command
+import Skype4Py
+
+
+@command(name="spam", permission='command.spam')
+def spam_command(chat, message, args, sender):
+    if len(args) < 2:
+        chat.SendMessage("Provide a user and message.")
+        return
+    user = args[0]
+    msg = message.replace('!spam {} '.format(user), '')
+    spam(user, msg)
+
+
+def spam(name, text, times=20):
+    skype = Skype4Py.Skype()
+    if not skype.Client.IsRunning:
+        skype.Client.Start()
+    skype.Attach()
+    temp = 0
+    while temp <= times:
+        skype.SendMessage(name, text)
+        temp += 1
