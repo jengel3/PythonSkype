8d0ac491d7afc81cea2b3bb0a5874d7b51c10539
Jake <jake0oo0andminecraft@gmail.com>
2014-06-26 14:30:34 -0500

    Add mcstatus listeners

--- commands/mcstatus.py
+++ commands/mcstatus.py
@@ -1,5 +1,9 @@
 import urllib
 import json
+import config
+import threading
+import Skype4Py
+from time import sleep
 
 from util.plugin import command
 
@@ -7,17 +11,41 @@ from util.plugin import command
 check = u'\u2713'
 nope = u'\u2717'
 
+LISTEN_API = 'http://xpaw.ru/mcstatus/status.json'
+
+listeners = []
+
 
 @command(name="mcstatus", help="Print Minecraft status")
 def choose(chat, message, args, sender):
     if len(args) == 1:
-        service = args[0]
-        if service in get_statuses():
-            chat.SendMessage(format_status(service))
-            return
+        if args[0].lower() == '--listen':
+            listeners.append(chat.Name)
+            conf = config.config()
+            config_operators = conf.get('mc_listens', [])
+            if chat.Name in config_operators:
+                chat.SendMessage("This chat is already listening to Minecraft service statuses.")
+                return
+            config_operators.append(chat.Name)
+            conf['mc_listens'] = config_operators
+            config.save(conf)
+        elif args[0].lower() == '--unlisten':
+            conf = config.config()
+            config_operators = conf.get('mc_listens', [])
+            if chat.Name not in config_operators:
+                chat.SendMessage("This chat is not currently listening to Minecraft service statuses.")
+                return
+            config_operators.remove(chat.Name)
+            conf['mc_listens'] = config_operators
+            config.save(conf)
         else:
-            chat.SendMessage("Service not found.")
-            return
+            service = args[0]
+            if service in get_statuses():
+                chat.SendMessage(format_status(service))
+                return
+            else:
+                chat.SendMessage("Service not found.")
+                return
     chat.SendMessage(format_status())
 
 
@@ -47,3 +75,49 @@ def format_status(check_service=None):
         if check_service is not None and service == check_service:
             return sb
     return sb
+
+
+def load_listeners():
+    conf = config.config()
+    chats = conf.get('mc_listens', [])
+    for chat in chats:
+        listeners.append(chat)
+
+
+def start_listener():
+    threading.Thread(target=listen)
+
+
+def get_chat_by_name(name):
+    skype = Skype4Py.Skype()
+    if not skype.Client.IsRunning:
+        skype.Client.Start()
+    skype.Attach()
+    for chat_id in skype.Chats:
+        if chat_id.Name == name:
+            return chat_id
+
+
+previous_statuses = {}
+
+
+def listen():
+    while True:
+        new_statuses = get_statuses()
+        changed = []
+        for service, status in new_statuses.items():
+            old_status = previous_statuses[service]
+            if status == old_status:
+                continue
+            else:
+                changed.append(service)
+        if len(changed) == 0:
+            return
+        else:
+            sb = 'The following services have changed status: \n'
+            for serv in changed:
+                sb += '{} is now {}\n'.format(serv, new_statuses['serv'])
+            for chat_id in listeners:
+                chat = get_chat_by_name(chat_id)
+                chat.SendMessage(sb)
+        sleep(60)
